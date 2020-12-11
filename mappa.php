50ae989bae67b5f460ac996a97c4c07b21f7a4ff
Andrew Grimes <61162113+ItHustleCoder@users.noreply.github.com>
2020-12-11 12:37:57 +0100

    Refacotring. Working with SESSION

--- mappa.php
+++ mappa.php
@@ -1,3 +1,18 @@
+
+<?php
+session_start();
+
+if(!isset($_SESSION['num'])) {
+  header("location: public/register.php");
+}
+
+$_SESSION['status'] = '1';
+
+ 
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -26,7 +41,8 @@
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
 	<script src="js/custom.maps.js?v<?php echo date('mdYhisa', time()); ?>"></script>
-	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCw5RwNMRim53BW4IgTVWu-1nHZM26730A&callback=initMap"> </script>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCw5RwNMRim53BW4IgTVWu-1nHZM26730A&callback=initMap"> </script>
+  
 
 </body>
 </html>
