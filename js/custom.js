bdc067ff3c8abc3a6871bdef81e122fe12adcf2c
FireFoxII <babinkuk@hotmail.com>
2020-12-11 19:06:13 +0100

    new update maps mobile

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,9 @@
 $( ".btn_top" ).click(function() {
-	$(".mapsPv").css("height", "80vh");
-	$(".btn_top").css("display", "none");
-	$(".btn_bottom").css("display", "flex");
-	$("#jsres").css("overflow-y", 'auto');
+	$(".mapsPv").removeClass('out');
+	$(".mapsPv").addClass('in');
 });
 
 $( ".btn_bottom" ).click(function() {
-	$(".mapsPv").css("height", "16vh");
-	$(".btn_top").css("display", "flex");
-	$(".btn_bottom").css("display", "none");
-	$("#jsres").css("overflow-y", 'hidden');
+	$(".mapsPv").removeClass('in');
+	$(".mapsPv").addClass('out');
 });
